e8618ce01c9e41769d60daa3c3cf4e2a8eed55ee
你的名字 <avatar.xu@gmail.com>
2023-07-10 23:31:46 +0200

    utf-8

--- helper.py
+++ helper.py
@@ -12,7 +12,7 @@ def init_logging(log_dir, log_level):
     
     Path(log_dir).mkdir(parents=True, exist_ok=True)
     log_path = os.path.join(log_dir, "smartcopy.log")
-    file_handler = TimedRotatingFileHandler(log_path, when="d")
+    file_handler = TimedRotatingFileHandler(log_path, when="d", encoding='UTF-8')
     file_handler.setLevel(log_level)
     file_handler.setFormatter(formatter)
 
